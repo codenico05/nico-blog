29c38fb2604a098356594b7f6249e8562c69dcb5
Efraim <efraimalhazov@gmail.com>
2023-10-01 23:59:53 +0300

    implemented trending cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
+import PostCard from '@/components/PostCard';
+import Header from '@/components/Header';
+import Trending from '@/components/Trending';
+
 export default function Home() {
   return (
     <main className="">
-      <div className="w-full px-20 flex items-center justify-center">
-        <h1
-          className="uppercase text-[2rem] lg:text-[9rem] md:text-[6rem] sm:text-[5rem]  cursor-default font-bold text-center relative md:before:h-0.5 before:w-full before:absolute before:bg-primary-dark before:dark:bg-primary-light md:before:top-7
-        md:after:h-0.5 before:h-[1px] after:h-[1px] after:w-full after:absolute after:bg-primary-dark after:dark:bg-primary-light md:after:bottom-7 after:left-0 sm:before:top-5 sm:after:bottom-5 before:top-1 after:bottom-1
-        "
-        >
-          Code Nico
-          <div />
-        </h1>
-      </div>
+      <Header text="Code Nico" />
+      <Trending />
     </main>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import ThemeToggle from '@/components/ThemeToggle';
 import NavLink from '@/components/NavLink';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className="xl:h-10 w-full flex justify-between px-[112px] py-[30px] md:h-8 sm:h-7">
       <h2 className="text-xl self-baseline cursor-pointer hover:text-green-400 dark:hover:text-red-400">
-        Logo
+        <Link href="/">Logo</Link>
       </h2>
       <ul className="flex flex-row items-center xl:gap-12 md:gap-8 self-baseline">
-        <NavLink pathName={'/'} text={'Blog'} />
+        <NavLink pathName={'/posts'} text={'Posts'} />
         <NavLink pathName={'/about'} text={'About'} />
         <NavLink pathName={'/newsletter'} text={'Newsletter'} />
         <li>
